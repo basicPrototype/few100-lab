7bca3faaa9cd18a46b75452fcb5bf5414917371f
basicPrototype <rmarquis@gmail.com>
2020-01-15 22:07:40 -0500

    red border on error

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,16 @@
-
 import './styles.css';
-console.log('Ready to Party With Some TypeScript!');
 
+// jeff sez get stuff we care about
+const billAmount = document.getElementById('billAmount') as HTMLInputElement;
+
+// handle events
+billAmount.addEventListener('input', handleTextEntry);
+
+function handleTextEntry() {
+    const enteredValue = billAmount.valueAsNumber;
+    if ( enteredValue < 0 ) {
+        billAmount.classList.add('inputError');
+    } else {
+        billAmount.classList.remove('inputError');
+    }
+}
