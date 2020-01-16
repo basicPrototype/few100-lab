1532e44e5bca6d258fab9e0f3d78abadfee1fbf6
basicPrototype <rmarquis@gmail.com>
2020-01-16 09:24:22 -0500

    basic layout and tip button handler

--- src/index.ts
+++ src/index.ts
@@ -2,15 +2,34 @@ import './styles.css';
 
 // jeff sez get stuff we care about
 const billAmount = document.getElementById('billAmount') as HTMLInputElement;
+const tipButtons = document.querySelectorAll('.tipButton') as NodeListOf<HTMLDivElement>;
+
 
 // handle events
 billAmount.addEventListener('input', handleTextEntry);
 
 function handleTextEntry() {
     const enteredValue = billAmount.valueAsNumber;
-    if ( enteredValue < 0 ) {
+    if (enteredValue < 0) {
         billAmount.classList.add('inputError');
     } else {
         billAmount.classList.remove('inputError');
     }
 }
+
+// add click handler to tip buttons
+tipButtons.forEach(btn => {
+    btn.addEventListener('click', handleTipButtonClick);
+});
+
+function handleTipButtonClick() {
+    const that = this as HTMLDivElement;
+    console.log(that.innerText);
+    tipButtons.forEach(btn => {
+        if (btn === this) {
+            that.removeEventListener('click', handleTipButtonClick);
+        }
+    });
+
+}
+console.log(tipButtons);
